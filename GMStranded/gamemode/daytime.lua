aa1999a8b9e33927806f8b7bd399d3ca765891d5
Robotboy655 <Robotboy655@f70b6dd3-2fee-9d9c-d5aa-21ae5d9e40f0>
2011-07-21 16:51:46 +0000

    Day/Night changes. May be the last commit before SEECREET

--- GMStranded/gamemode/daytime.lua
+++ GMStranded/gamemode/daytime.lua
@@ -6,7 +6,7 @@ TargetPattern = DayLight
 CurrentPattern = DayLight
 
 DayTime = 355 -- Day: 710 seconds, Night: 730 seconds
-NightTime = 1085
+NightTime = 1065
 Time = DayTime + 1
 
 IsNight = false
@@ -14,18 +14,12 @@ IsNight = false
 if (CLIENT) then
 	timer.Create("DayTime.TimerClient", 1, 0, function()
 		Time = Time + 1
+		if(Time > 1440) then Time = 0 end
 	end)
 elseif (SERVER) then
-	local light_environments = ents.FindByClass('light_environment')
-	if (#light_environments > 0) then
-		for _,light in pairs(light_environments) do -- Initial fade	
-			light:Fire('FadeToPattern', string.char(CurrentPattern), 0)
-			light:Activate()    
-		end
-	end
-
 	timer.Create("DayTime.TimerServer", 1, 0, function()
 		Time = Time + 1
+		if(Time > 1440) then Time = 0 end
 		local suns = ents.FindByClass("env_sun")
 		if (#suns > 0) then
 			for id, sun in pairs(suns) do
@@ -37,21 +31,21 @@ elseif (SERVER) then
 		local light_environments = ents.FindByClass('light_environment')
 		if (#light_environments > 0) then
 			for _,light in pairs(light_environments) do
-				if ((Time < DayTime or Time > NightTime) and TargetPattern != NightLight) then
+				if (Time < DayTime or Time > NightTime) then
 					TargetPattern = NightLight
 					IsNight = true
-				elseif ((Time >= DayTime or Time <= NightTime) and TargetPattern != DayLight) then
+				elseif (Time > DayTime or Time < NightTime) then
 					TargetPattern = DayLight
 					IsNight = false
 				end
-				
+
 				if ((Time % 3) == 0 and TargetPattern != CurrentPattern) then
 					if (TargetPattern > CurrentPattern) then
 						CurrentPattern = math.Clamp(CurrentPattern + 1, NightLight, DayLight)
 					else
 						CurrentPattern = math.Clamp(CurrentPattern - 1, NightLight, DayLight)
 					end
-				
+
 					light:Fire('FadeToPattern', string.char(CurrentPattern), 0)
 					light:Activate()    
 				end
