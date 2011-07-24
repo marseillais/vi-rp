8a6825b6202ac2143df22fa17a02b58939cf6061
Robotboy655 <Robotboy655@f70b6dd3-2fee-9d9c-d5aa-21ae5d9e40f0>
2011-07-24 17:39:37 +0000

    Fix for grenades!

--- trunk/deathrun/entities/entities/ent_smokegrenade/shared.lua
+++ trunk/deathrun/entities/entities/ent_smokegrenade/shared.lua
@@ -1,13 +1,9 @@
 ENT.Type 			= "anim"
-ENT.PrintName		= "Smoke Grenade"
-ENT.Author			= "-NEGI-"
-ENT.Contact			= "gic12@rambler.ru"
+ENT.PrintName		= ""
+ENT.Author			= ""
+ENT.Contact			= ""
 ENT.Purpose			= ""
-ENT.Instructions	= "DEVS DEVS DEVS"
-
-function ENT:SetupDataTables()  
-	self:DTVar("Boal", 0, "Explode")
-end 
+ENT.Instructions	= ""
 
 function ENT:OnRemove()
 end
@@ -21,18 +17,4 @@ function ENT:PhysicsCollide(data, phys)
 	end
 	local impulse = -data.Speed * data.HitNormal * 0.4 + (data.OurOldVelocity * -0.6)
 	phys:ApplyForceCenter(impulse)
-
-	if not self.Collide then self.Collide = false end
-	if self.Collide then return end
-
-	timer.Simple(1, function()
-		if not self.Entity then return end
-		if not IsFirstTimePredicted() then return end
-
-		self.Entity:EmitSound(Sound("BaseSmokeEffect.Sound"))
-		self.Entity:Fire("kill", "", 5)
-
-		self.Entity:SetDTBool(0, true)
-	end)
-	self.Entity.Collide = true
 end
